180320157edde2c5d22f5ed907d5deffd0ac2142
ArttuKorpela <arttu.korpela01@gmail.com>
2024-04-18 20:48:13 +0300

    database changes

--- db/db.go
+++ db/db.go
@@ -6,7 +6,6 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
@@ -15,7 +14,7 @@ import (
 func StartDatabase(ctx context.Context) (*mongo.Collection, error) {
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI("mongodb+srv://arttukorpela01:testi123!@distributedsystemstesti.a3bqykr.mongodb.net/?retryWrites=true&w=majority&appName=DistributedSystemsTesting").SetServerAPIOptions(serverAPI)
-	client, err := mongo.Connect(ctx, clientOptions)
+	client, err := mongo.Connect(ctx, opts)
 	if err != nil {
 		return nil, err
 	}
@@ -25,9 +24,9 @@ func StartDatabase(ctx context.Context) (*mongo.Collection, error) {
 		  panic(err)
 		}
 	  }()
-	  // Send a ping to confirm a successful connection
-	  if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
-		panic(err)
-	  }
-	  fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+	// Send a ping to confirm a successful connection
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+	panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
 }
